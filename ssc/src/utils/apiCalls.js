a66775fd176d50ab4393336b45c844d1deb20976
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-22 07:49:10 +0000

    updated pkg json with axios, cypress and other requird pkgs + added components folder

---
+++ ssc/src/utils/apiCalls.js
@@ -0,0 +1,63 @@
+const axios = require( 'axios' );
+const { BASE_URL } = require( '../utils/urls' );
+
+const translateMethod = ( method ) => {
+    const methodTranslation = {
+        'get': 'fetch',
+        'post': 'create',
+        'patch': 'update',
+        'put': 'update',
+        'delete': 'delete'
+    };
+    return methodTranslation[ method ];
+};
+const makeAPICalls = ( { url, reqObjectKey, params, data, method, multiRes, cancelToken } ) => {    
+    const instance = axios.create( {
+        baseURL: `${ BASE_URL }`,
+        url,
+        params,
+        method,
+        data,
+        cancelToken
+    } );    
+    const action = translateMethod( method );    
+    const genericMsg = `Could not ${ action } ${ reqObjectKey }. Contact Support.`;
+
+    if ( method === 'delete' ) {
+        return new Promise ( ( resolve, reject ) => {
+            instance.request( )
+                .then( ( { status } ) => {                    
+                    resolve( status );
+                } )
+                .catch( ( { response: { data } } ) => {                  
+                    const err = data.error ? data.error : genericMsg; 
+                    reject( err ) ;
+                } );
+        } );
+    } else if ( multiRes ) {
+        return new Promise ( ( resolve, reject ) => {
+            instance.request( )
+                .then( ( { data } ) => {                    
+                    resolve( data );
+                } )
+                .catch( ( res ) => {     
+                    const err = res.data ? res.data.error : genericMsg;                     
+                    reject( err ) ;
+                } );
+        } );
+    } else {
+        return new Promise ( ( resolve, reject ) => {
+            instance.request( )
+                .then( ( { data: { [ reqObjectKey ]: value } } ) => {                    
+                    resolve( value );
+                } )
+                .catch( ( { response: { data } } ) => {                  
+                    const err = data.error ? data.error : genericMsg; 
+                    reject( err ) ;
+                } );
+        } );
+    }
+    
+};
+
+module.exports = { makeAPICalls };
---
+++ ssc/src/utils/url.js
@@ -0,0 +1,6 @@
+exports.BASE_URL = 'https://localhost:5000/api';
+exports.USERS_EP = 'users';
+exports.LOGIN_EP = 'login';
+exports.DELETE_USER_EP = 'deleteUser';
+exports.INVITES_EP = 'invites';
+exports.WORKSPACES_EP = 'workspaces';
