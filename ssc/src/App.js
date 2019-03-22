1bcdcf7fd2e774cfde8aa0227cfdfcca7026292c
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-22 12:58:07 +0000

    delete workspace done

--- ssc/src/App.js
+++ ssc/src/App.js
@@ -3,15 +3,16 @@ import UserDashboard from './components/UserDashboard';
 
 class App extends Component {
     state={
+        // pls do note remove
         // username: null,
-        username: 'Shruminator'
+        //  username: 'Shruminator'
     }
     render() {
         const { username } = this.state;
         return (
             <div className="App">
                 <h1>SSC</h1>
-                {/* 
+                {/* Added by shumanator....pls do not remove
                     {username && <UserDashboard username = {username}/>}
                 */}
             </div>
