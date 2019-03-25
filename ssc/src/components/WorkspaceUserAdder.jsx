3848527c869a2658dc0d36f59e6fd72f73fc6b13
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-25 17:13:05 +0000

    aesthetic changes

--- ssc/src/components/WorkspaceUserAdder.jsx
+++ ssc/src/components/WorkspaceUserAdder.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button, ListGroup } from 'react-bootstrap';
 
-const WorkspaceUserAdder = ( { handleAddUser, handleNewUserChange, newUser, filteredUsers, handleItemClick } ) => {         
+const WorkspaceUserAdder = ( { handleAddUser, handleNewUserChange, newUser, filteredUsers, handleItemClick } ) => {   
+    const croppedList = filteredUsers.slice( 0,5 );      
     return (
         <Form onSubmit={handleAddUser}>
             <Form.Group controlId="formBasicUserName">
                 <Form.Control type="text" value = {newUser} placeholder="Enter new User to add" onChange={handleNewUserChange}/>
-                <ListGroup>                    
-                    {
-                        filteredUsers.map( user => {
+                {newUser !== '' && <ListGroup>                    
+                    {                                            
+                        croppedList.map( user => {
                             return ( <ListGroup.Item key={user.username} onClick={handleItemClick}>{user.username}</ListGroup.Item> );
                         } )
                     }
-                </ListGroup>                
+                </ListGroup> }
             </Form.Group>
             <Button variant="primary" type="submit" disabled={newUser === ''}>
                 Add
