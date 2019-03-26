a4b14a322b9519e60a5046c7f45d45f59c0d9292
yuvaraj kadam <yuvarajkadam@macusers-iMac-2.local>
2019-03-26 09:50:45 +0000

    get files for workspace on FE setup

--- ssc/src/App.js
+++ ssc/src/App.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import UserDashboard from './components/UserDashboard';
 
 class App extends Component {
-    state={
+    state = {
         // pls do note remove
         // username: null,
-        username: 'Shruminator'
-    }
+        username: 'OllieOllie123'
+    };
     render() {
         const { username } = this.state;
         return (
             <div className="App">
                 <h1>SSC</h1>
                 {/* Added by shumanator....pls do not remove*/}
-                {username && <UserDashboard username = {username}/>}
-                
+                {username && <UserDashboard username={username} />}
             </div>
         );
     }
