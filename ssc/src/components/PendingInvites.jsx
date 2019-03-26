fc802a5fa31036afccb929e85d81d57cef82588b
Fatmeh Shuman <fatoumeh@macusers-MacBook-Pro.local>
2019-03-26 12:24:29 +0000

    accept/reject invites

--- ssc/src/components/PendingInvites.jsx
+++ ssc/src/components/PendingInvites.jsx
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import { makeAPICalls } from '../utils/apiCalls';
-import { Dropdown, Alert } from 'react-bootstrap';
+import { Dropdown, Alert, Button } from 'react-bootstrap';
 
 class PendingInvites extends Component {
   state = {
       invites: [],
-      isClicked: false
+      invitesError: ''
   };
   render() {
-      const { invites } = this.state;
+      const { invites, invitesError } = this.state;
 
       return (
           <div className="invitesDiv">
+              {invitesError !== '' && <Alert variant="danger">{invitesError}</Alert>}
               {invites.length > 0 
                   ? <Dropdown>
                       <Dropdown.Toggle className="invitesList" variant="warning" id="dropdown-custom-1">View Invites</Dropdown.Toggle>
                       <Dropdown.Menu>
                           {invites && invites.map( invite => {
-                              return <Dropdown.Item key={invite.workspace} eventKey={invite.workspace}>{invite.invited_by} added you to {invite.workspace}</Dropdown.Item>;
+                              return <>
+                                <Dropdown.Item key={invite.workspace} eventKey={invite.workspace}>
+                                    {invite.invited_by} added you to {invite.workspace}
+                                    <span className="acceptRejectSpan" onClick={() => this.acceptReject( invite.workspace, 'True' )}>✅</span>
+                                    <span className="acceptRejectSpan" onClick={() => this.acceptReject( invite.workspace, 'False' )}>❌</span>                                    
+                                </Dropdown.Item>
+                              </>;
+                              
                           } )
                           }            
                       </Dropdown.Menu>
@@ -30,6 +38,29 @@ class PendingInvites extends Component {
   componentDidMount() {
       this.fetchInvites();
   }
+
+  acceptReject = ( workspace, accept ) => {
+      const { username } = this.props;
+      const apiObj = {
+          url: `/invites/${ username }`,
+          reqObjectKey: 'invite_processed',
+          method: 'post',
+          data: { accept, workspace }
+      };
+      makeAPICalls( apiObj )
+          .then( ( invite_processed ) => {
+              if ( invite_processed ) {
+                  this.fetchInvites();  
+                  this.props.handleRefresh();
+              } else {
+                  this.setState( { invitesError: 'Could not accept/reject invite' } ); 
+              }              
+          } )
+          .catch( ( err ) => {
+              this.setState( { invitesError: 'Could not accept/reject invite' } ); 
+          } );
+  }
+
   fetchInvites = () => {
       const { username } = this.props;
 
@@ -40,10 +71,10 @@ class PendingInvites extends Component {
       };
       makeAPICalls( apiObj )
           .then( ( invites ) => {
-              this.setState( { invites } );                
+              this.setState( { invites, invitesError: '' } );                
           } )
           .catch( ( err ) => {
-              this.setState( { invites: [] } ); 
+              this.setState( { invites: [], invitesError: 'Could not get invites' } ); 
           } );
   };
 
--- ssc/src/components/UserDashboard.jsx
+++ ssc/src/components/UserDashboard.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Workspaces from './Workspaces';
 import PendingInvites from './PendingInvites';
+import { Row, Col } from 'react-bootstrap';
 
-const UserDashboard = ( { location: { state: { username } } } ) => {    
-    return (
-        <div className="overallDashboard">
+class UserDashboard extends Component{    
+    state = {
+        refreshWorkspaces: false
+    }
+
+    handleRefresh = () => {
+        this.setState( { refreshWorkspaces: true } );
+    }
+    
+    render () {
+        const { refreshWorkspaces } = this.state;
+        const { location: { state: { username } } } = this.props;        
+        return (
+            <>
             <h1>Hello {username}</h1>
-            <PendingInvites username={username} />
-            <Workspaces username={username} />
-        </div>
-    );
-};
+            <Row className="overallDashboard">
+                <Col/>
+                <Col>
+                    <PendingInvites username={username} handleRefresh={this.handleRefresh}/>
+                </Col>
+            </Row>            
+            <Workspaces username={username} refreshWorkspaces={refreshWorkspaces}/>                          
+            </>
+        );
+    }
+    
+}
 
 UserDashboard.propTypes = {
     location: PropTypes.object,
